f93f530e5703b5bc949a5ae2fa97f44e57ec478f
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-03-22 14:00:04 +0800

    [u] UpdateTables

---
+++ Code/IDM.Api/IDM.Infrastructure/Migrations/20230322055940_UpdateTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IDM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "SecurityGroup_TRN",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)",
+                oldMaxLength: 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "SecurityGroup_MST",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)",
+                oldMaxLength: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "SecurityGroup_TRN",
+                type: "nvarchar(1)",
+                maxLength: 1,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "SecurityGroup_MST",
+                type: "nvarchar(1)",
+                maxLength: 1,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
