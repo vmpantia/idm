f3ef48f034ad3e128b0816a51336a5eb2084cdb7
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-03-22 10:57:24 +0800

    [+] Add SideBar

--- Code/IDM.Web/IDM.Web/src/app/app-routing.module.ts
+++ Code/IDM.Web/IDM.Web/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SecurityGroupComponent } from './components/security-group/security-group.component';
+
+const routes: Routes = [
+  {path: 'securitygroup', component:SecurityGroupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Code/IDM.Web/IDM.Web/src/app/app.module.ts
+++ Code/IDM.Web/IDM.Web/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SecurityGroupComponent } from './components/security-group/security-group.component';
 import { AddEditSGComponent } from './components/security-group/add-edit-sg/add-edit-sg.component';
-import { ShowSGListComponent } from './components/security-group/show-sg-list/show-sg-list.component';
 import { ComputerComponent } from './components/computer/computer.component';
 
 @NgModule({
@@ -14,7 +13,6 @@ import { ComputerComponent } from './components/computer/computer.component';
     AppComponent,
     SecurityGroupComponent,
     AddEditSGComponent,
-    ShowSGListComponent,
     ComputerComponent
   ],
   imports: [
