c3b9d22148c3fc90cdafcf51861f222b75e054f0
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-04-02 18:49:47 +0800

    [+] Add SGList View

---
+++ Code/IDM/IDM.Web-MVC/Controllers/SecurityGroupController.cs
@@ -0,0 +1,27 @@
+﻿using IDM.Business.Contractors;
+using IDM.Web_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDM.Web_MVC.Controllers
+{
+    public class SecurityGroupController : Controller
+    {
+        private readonly ISecurityGroupService _sg;
+        public SecurityGroupController(ISecurityGroupService sg)
+        {
+            _sg = sg;
+        }
+
+        public async Task<IActionResult> SGList()
+        {
+            var result = await _sg.GetSGsAsync();
+
+            var model = new SecurityGroupViewModel
+            {
+                sgList = result.ToList(),
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Code/IDM/IDM.Web-MVC/Models/SecurityGroupViewModel.cs
@@ -0,0 +1,10 @@
+﻿using IDM.Business.Models.DTOs;
+
+namespace IDM.Web_MVC.Models
+{
+    public class SecurityGroupViewModel
+    {
+        public List<SecurityGroupDTO> sgList { get; set; }
+        public SecurityGroupDTO sgInfo { get; set; }
+    }
+}
