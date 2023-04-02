49554b68e7d02e0d2c258ff57782f27baf003966
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-04-02 18:29:02 +0800

    [+] Add IDM Web and IDM Infrastructure

---
+++ Code/IDM/IDM.Infrastructure/DataAccess/Entities/EmailAddress_TRN.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Core;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace IDM.Infrastructure.DataAccess.Entities
+{
+    [PrimaryKey(nameof(RequestID), nameof(Number))]
+    public class EmailAddress_TRN
+    {
+        //Request Details
+        public string RequestID { get; set; }
+        public string Number { get; set; }
+
+        //EmailAddress Details
+        [MaxLength(50)]
+        public string EmailAddress { get; set; }
+        public Guid RelationID { get; set; } /*Security Group, Account InternalID*/
+        public int OwnerType { get; set; }
+        public int EmailType { get; set; }
+        public int PrimaryFlag { get; set; }
+
+        //Common Details
+        public int Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ Code/IDM/IDM.Infrastructure/DataAccess/IDMDbContext.cs
@@ -0,0 +1,16 @@
+﻿using IDM.Infrastructure.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDM.Infrastructure.DataAccess
+{
+    public class IDMDbContext : DbContext
+    {
+        public IDMDbContext(DbContextOptions<IDMDbContext> options) : base(options) { }
+
+        public virtual DbSet<Request_LST> Request_LST { get; set; }
+        public virtual DbSet<SecurityGroup_MST> SecurityGroup_MST { get; set; }
+        public virtual DbSet<SecurityGroup_TRN> SecurityGroup_TRN { get; set; }
+        public virtual DbSet<EmailAddress_MST> EmailAddress_MST { get; set; }
+        public virtual DbSet<EmailAddress_TRN> EmailAddress_TRN { get; set; }
+    }
+}
